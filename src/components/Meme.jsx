618c1015f9ab9a12dee84faeba44515daa2d8c48
Jason Cassella <jasoncassella1@gmail.com>
2022-12-13 00:28:35 -0500

    now using state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,49 @@
+import React from 'react';
+import memesData from '../memesData.js';
+
 export default function Meme() {
+	/**
+	 * Challenge: Update our state to save the meme-related
+	 * data as an object called `meme`. It should have the
+	 * following 3 properties:
+	 * topText, bottomText, randomImage.
+	 *
+	 * The 2 text states can default to empty strings for now,
+	 * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
+	 *
+	 * Next, create a new state variable called `allMemeImages`
+	 * which will default to `memesData`, which we imported above
+	 *
+	 * Lastly, update the `getMemeImage` function and the markup
+	 * to reflect our newly reformed state object and array in the
+	 * correct way.
+	 */
+
+	const [meme, setMeme] = React.useState({
+		topText: '',
+		bottomText: '',
+		randomImage: 'http://i.imgflip.com/1bij.jpg',
+	});
+
+	const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+	function getMemeImage() {
+		const memesArray = allMemeImages.data.memes;
+		const randomNumber = Math.floor(Math.random() * memesArray.length);
+		const randomMeme = memesArray[randomNumber].url;
+		setMeme(prevMeme => ({ ...prevMeme, randomImage: randomMeme }));
+	}
+
 	return (
 		<main>
-			<form className='form'>
-				<input type='text' placeholder='top text' className='form--input' />
-				<input type='text' placeholder='bottom text' className='form--input' />
-				<button className='form--button'>Get a new meme image 🖼</button>
-			</form>
+			<div className='form'>
+				<input type='text' placeholder='Top text' className='form--input' />
+				<input type='text' placeholder='Bottom text' className='form--input' />
+				<button className='form--button' onClick={getMemeImage}>
+					Get a new meme image 🖼
+				</button>
+			</div>
+			<img src={meme.randomImage} className='meme--image' />
 		</main>
 	);
 }
