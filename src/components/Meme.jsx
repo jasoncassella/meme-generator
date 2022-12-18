99d2b94abc57b2a3a03e1106f44ffe9181004862
Jason Cassella <jasoncassella1@gmail.com>
2022-12-18 14:47:33 -0500

    dynamically generating meme text

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -18,6 +18,13 @@ export default function Meme() {
 
 	const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setMeme(prevMeme => {
+			return { ...prevMeme, [name]: value };
+		});
+	}
+
 	function getMemeImage() {
 		const memesArray = allMemeImages.data.memes;
 		const randomNumber = Math.floor(Math.random() * memesArray.length);
@@ -31,16 +38,30 @@ export default function Meme() {
 	return (
 		<main>
 			<div className='form'>
-				<input type='text' placeholder='Top text' className='form--input' />
-				<input type='text' placeholder='Bottom text' className='form--input' />
+				<input
+					type='text'
+					placeholder='Top text'
+					className='form--input'
+					onChange={handleChange}
+					name='topText'
+					value={meme.topText}
+				/>
+				<input
+					type='text'
+					placeholder='Bottom text'
+					className='form--input'
+					onChange={handleChange}
+					name='bottomText'
+					value={meme.bottomText}
+				/>
 				<button className='form--button' onClick={getMemeImage}>
 					Get a new meme image 🖼
 				</button>
 			</div>
 			<div className='meme'>
 				<img src={meme.randomImage} className='meme--image' />
-				<h2 className='meme--text top'>One does not simply</h2>
-				<h2 className='meme--text bottom'>Walk into Mordor</h2>
+				<h2 className='meme--text top'>{meme.topText}</h2>
+				<h2 className='meme--text bottom'>{meme.bottomText}</h2>
 			</div>
 		</main>
 	);
